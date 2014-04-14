16c75f67df7f5879ccaf903a58c1b876e4baa8cd
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-04-14 16:01:43 +0200

    use jq to create database user

--- .vagrant/libs/mysql.sh
+++ .vagrant/libs/mysql.sh
@@ -4,8 +4,11 @@
 source /vagrant/.vagrant/libs/apt.sh
 
 function configureMysql() {
-  debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
-  debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
+  local username=$(jq ".provision.database.username" /vagrant/vagrant.json)
+  local password=$(jq ".provision.database.password" /vagrant/vagrant.json)
+
+  debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password $username"
+  debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password $password"
 }
 
 function installMysql() {

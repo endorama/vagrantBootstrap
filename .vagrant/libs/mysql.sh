4045ce5e93f2fe74c4182d2c00fc782227793ae2
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-04-14 17:06:04 +0200

    fix mysql configuration

--- .vagrant/libs/mysql.sh
+++ .vagrant/libs/mysql.sh
@@ -4,10 +4,9 @@
 source /vagrant/.vagrant/libs/apt.sh
 
 function configureMysql() {
-  local username=$(jq ".provision.database.username" /vagrant/vagrant.json)
   local password=$(jq ".provision.database.password" /vagrant/vagrant.json)
 
-  debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password $username"
+  debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password $password"
   debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password $password"
 }
 
@@ -16,9 +15,8 @@ function installMysql() {
 }
 
 function createDatabase() {
-  local username=$(jq ".provision.database.username" /vagrant/vagrant.json)
   local password=$(jq ".provision.database.password" /vagrant/vagrant.json)
   local name=$(jq ".provision.database.name" /vagrant/vagrant.json)
 
-  mysql -u $username -p$password -e "create database $name;"
+  mysql -u root -p$password -e "create database $name;"
 }

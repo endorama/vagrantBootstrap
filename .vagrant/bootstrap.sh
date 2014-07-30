f03affcfceab6ad64b063525f72787aec6e8f2c8
terminalia <mistwork@gmail.com>
2014-07-30 12:48:37 +0200

    Update bootstrap.sh

--- .vagrant/bootstrap.sh
+++ .vagrant/bootstrap.sh
@@ -17,33 +17,16 @@ copyBashFunctions
 # mysql
 configureMysql 
 installMysql
-oauth_name=$(getConfig ".database_oauth.name")
-createDatabase $oauth_name
-resource_name=$(getConfig ".database_resource.name")
-createDatabase $resource_name
-configureMysqlWorkbench
+
 
 # nodejs
 installNodeJsBin
 installNpmGlobalModules
 
 ### CUSTOM MACHINE SETTINGS ####################################################
-
-#Create required db tables
-oauth_db_name=$(getConfig '.database_oauth.name')
-oauth_db_passwd=$(getConfig '.database.password')
-for i in $(ls /vagrant/oauth-server/database/sql); do
-  echo "Dumping $i into $oauth_db_name"
-  mysql -u root -p$oauth_db_passwd $oauth_db_name < /vagrant/oauth-server/database/sql/$i
-done
-
-oauth_db_name=$(getConfig '.database_resource.name')
-oauth_db_passwd=$(getConfig '.database.password')
-for i in $(ls /vagrant/resource-server/database/sql); do
-  echo "Dumping $i into $oauth_db_name"
-  mysql -u root -p$oauth_db_passwd $oauth_db_name < /vagrant/resource-server/database/sql/$i
-done
-
-#Install required NodeJS modules
-cd /vagrant/oauth-server && npm install
-cd /vagrant/resource-server && npm install
+if [ ! -f $vagrant_root/provision.sh ];
+    then echo "### Missing provision.sh file. Base machine built. ###"
+else
+    echo "### CUSTOM MACHINE INSTALL...###"
+    source $vagrant_root/provision.sh
+fi
